342647e7b4261e0a8109a79218ccd2837f403dc5
Owen Van Vooren <ovsquared@icloud.com>
2024-10-08 14:14:22 -0500

    AutoUpdate

--- service-worker.js
+++ service-worker.js
@@ -4,31 +4,40 @@ const urlsToCache = [
   '/styles.css',
   '/apple-touch-icon.png',
   '/index.html',
-  'favicon.ico',
-  'firebaseconfig.js',
-  'dev.html',
-  'home.html',
-  'header.html',
-  'header.js',
-  'photography.html',
-  'mascot.png',
-  'mascotblue.png',
-  'manifest.json',
-  'service-worker.js'
+  '/favicon.ico',
+  '/firebaseconfig.js',
+  '/dev.html',
+  '/home.html',
+  '/header.html',
+  '/header.js',
+  '/photography.html',
+  '/mascot.png',
+  '/mascotblue.png',
+  '/manifest.json',
+  '/service-worker.js'
 ];
 
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
+    console.log('Service Worker installing.');
+    event.waitUntil(
+      caches.open(CACHE_NAME).then((cache) => {
+        console.log('Caching files');
+        return cache.addAll(urlsToCache);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    console.log('Fetch intercepted for:', event.request.url);
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request).then((networkResponse) => {
+          return caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, networkResponse.clone());
+            return networkResponse;
+          });
+        });
+      })
+    );
+  });
+  
