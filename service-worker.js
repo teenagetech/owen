fb1484dc218217212519a3ab8e482384ad568981
Owen Van Vooren <ovsquared@icloud.com>
2024-10-15 21:11:58 -0500

    AutoUpdate

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,19 @@
 const CACHE_NAME = 'owen-cache';
 const urlsToCache = [
-  '/',
-  '/styles.css',
-  '/apple-touch-icon.png',
-  '/index.html',
-  '/favicon.ico',
-  '/firebaseconfig.js',
-  '/dev.html',
-  '/home.html',
-  '/header.html',
-  '/header.js',
-  '/photography.html',
-  '/mascot.png',
-  '/mascotblue.png',
-  '/manifest.json',
-  '/service-worker.js'
+  '../',
+  '../styles.css',
+  '../apple-touch-icon.png',
+  '../index.html',
+  '../favicon.ico',
+  '../firebaseconfig.js',
+  '../dev.html',
+  '../header.html',
+  '../header.js',
+  '../photography.html',
+  '../mascot.png',
+  '../mascotblue.png',
+  '../manifest.json',
+  '../service-worker.js'
 ];
 
 self.addEventListener('install', (event) => {
